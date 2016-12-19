8b4c100e56c0f2bcdb1b0bf28e443130b409de31
amimelia <meliaamirani@gmail.com>
2016-12-19 12:36:28 -0800

    ...

---
+++ ChallengeMeServer/ChallengeMeServer/Controllers/ChallangeMeAmiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChallengeMeServer.Controllers
+{
+    public class ChallangeMeAmiController : ApiController
+    {
+        // GET: api/ChallangeMeAmi
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ChallangeMeAmi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ChallangeMeAmi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ChallangeMeAmi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ChallangeMeAmi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
