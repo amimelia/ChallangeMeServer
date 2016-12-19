11b6d2eec5b83b1e891d1ea7ba862c08e0b92277
amimelia <meliaamirani@gmail.com>
2016-12-19 12:23:11 -0800

    test commit

--- ChallengeMeServer/ChallengeMeServer/Controllers/ChallengeMeTesterController.cs
+++ ChallengeMeServer/ChallengeMeServer/Controllers/ChallengeMeTesterController.cs
@@ -12,7 +12,7 @@ namespace ChallengeMeServer.Controllers
         // GET: api/ChallengeMeTester
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "amirani", "ako" };
         }
 
         // GET: api/ChallengeMeTester/5
