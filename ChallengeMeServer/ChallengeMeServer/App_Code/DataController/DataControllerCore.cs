e4dee70c715a3a86acc48b54e7beb7045873ef8a
amimelia <meliaamirani@gmail.com>
2016-12-21 11:35:05 -0800

    added data controller

---
+++ ChallengeMeServer/ChallengeMeServer/App_Code/DataController/DataControllerCore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeMeServer.App_Code.DataController
+{
+    public class DataControllerCore
+    {
+    }
+}
